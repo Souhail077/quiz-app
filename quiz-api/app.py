c654ec077b43be13a94e7f09a4199f831e11222e
Zayn <mohamed99zayn@gmail.com>
2022-12-16 11:19:58 +0100

    Add quiz-info api

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
 
 if __name__ == "__main__":
     app.run()
